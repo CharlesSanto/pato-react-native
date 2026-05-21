a005e2c44988afcfec24be342db92696e2c9e6f5
unknown <2412130024@ACADEMICO.IESB.BR>
2026-05-21 20:06:20 -0300

    fix: make changes to run migrations correctly

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-npx drizzle-kit generate
+echo "Waiting for postgres..."
 
+until pg_isready -h postgres -p 5432 -U app_user
+do
+  sleep 2
+done
+
+echo "Postgres is ready"
+
+npx drizzle-kit generate
 npx drizzle-kit migrate
 
 npm run dev
